a0e514c8370e1b6edfe90304c2409695bbd65589
Manan Singla <Manansingla4@gmail.com>
2020-02-01 15:30:33 +0530

    Restructured the classes to more general classes

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,33 +1,14 @@
 public class Coordinate {
     private int x;
     private int y;
-    private boolean marked;
-    private String symbol;
 
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
-        this.marked = false;
     }
 
     public int getX() {
         return this.x;
-    }
-
-    public void setMarked() {
-        marked = true;
-    }
-
-    public boolean isMarked() {
-        return marked;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
     }
 
     public int getY() {
