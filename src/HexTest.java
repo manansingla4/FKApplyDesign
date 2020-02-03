fc277958dd030b8bda280cc6fa36f39f8da6fbe7
Manan Singla <Manansingla4@gmail.com>
2020-02-03 11:33:00 +0530

    Added Test Cases

--- src/HexTest.java
+++ src/HexTest.java
@@ -13,6 +13,10 @@ public class HexTest extends TestCase {
         assertTrue(game.isValid(new Coordinate(1, 1)));
         assertFalse(game.isValid(new Coordinate(100, 100)));
         assertFalse(game.isValid(new Coordinate(20, 20)));
+        assertTrue(game.isValid(new Coordinate(0, 2)));
+        assertTrue(game.isValid(new Coordinate(1, 1)));
+        assertFalse(game.isValid(new Coordinate(1, -1)));
+        assertTrue(game.isValid(new Coordinate(2, 0)));
     }
 
     void checkDiagonal() {
@@ -23,6 +27,12 @@ public class HexTest extends TestCase {
         game.setMarked(new Coordinate(1, 1), player.getSymbol());
         game.setMarked(new Coordinate(2, 0), player.getSymbol());
         assertTrue(game.checkWinner(player.getSymbol()));
+        game = new HexTicTacToeGame(3, 1);
+        game.setMarked(new Coordinate(1, 3), player.getSymbol());
+        game.setMarked(new Coordinate(2, 2), player.getSymbol());
+        game.setMarked(new Coordinate(3, 1), player.getSymbol());
+        game.setMarked(new Coordinate(0, 4), player.getSymbol());
+        assertTrue(game.checkWinner(player.getSymbol()));
     }
 
     void checkRow() {
@@ -32,6 +42,12 @@ public class HexTest extends TestCase {
         game.setMarked(new Coordinate(0, 2), player.getSymbol());
         game.setMarked(new Coordinate(0, 4), player.getSymbol());
         game.setMarked(new Coordinate(0, 6), player.getSymbol());
+        assertTrue(game.checkWinner(player.getSymbol()));
+        game = new HexTicTacToeGame(3, 1);
+        game.setMarked(new Coordinate(4, 2), player.getSymbol());
+        game.setMarked(new Coordinate(4, 4), player.getSymbol());
+        assertFalse(game.checkWinner(player.getSymbol()));
+        game.setMarked(new Coordinate(4, 6), player.getSymbol());
         assertTrue(game.checkWinner(player.getSymbol()));
     }
 
